7e7cf08aabac21787a07fd1be6d0ef469bcac4c9
LivyyX <oliwialankiewicz@gmail.com>
2024-03-30 15:37:28 +0100

    it is broken :(

--- blog/forms.py
+++ blog/forms.py
@@ -1,7 +1,11 @@
 from django import forms
 from .models import Post
 
-class ImgForm(forms.ModelForm):
+class PostForm(forms.ModelForm):
+    
+    title = forms.CharField(help_text='POMOCY!')
+    
     class Meta:
         model=Post
-        fields =['image']
+        fields =['title','text','image']
+        
